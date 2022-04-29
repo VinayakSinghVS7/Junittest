c01dc31e97d1d5da0422615ee16b35c2e148e445
VINAYAK SINGH <VINAYAK SINGH@10.12.37.249>
2022-04-30 01:51:00 +0530

    JunitTest

---
+++ src/jUnitTestPackage/testAddStrings.java
@@ -0,0 +1,15 @@
+package jUnitTestPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAddStrings {
+
+	@Test
+	public void test() {
+		jUnitFunctions jUnit=new jUnitFunctions();
+		String result=jUnit.AddStrings("abc","def");
+		assertEquals("abcdef",result);	}
+
+}
